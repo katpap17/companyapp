4c56a373b6dfce7f4ca11b642cb4c74e7f2136d1
Katerina <kat.papapos@gmail.com>
2024-01-20 19:54:20 +0200

    Read db params from env

--- database/db.go
+++ database/db.go
@@ -1,13 +1,20 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/katpap17/companyapp/utils"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Init() (*gorm.DB, error) {
-	dsn := "host=127.0.0.1 user=postgres password=mysecretpassword dbname=companydb sslmode=disable"
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbUser, dbPass, dbName)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		utils.Logger.Error(err)
